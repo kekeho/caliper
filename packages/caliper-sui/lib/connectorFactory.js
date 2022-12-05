4d7181874ce09e8f63aae8c6dbcc2174600a5b9f
Hiroki TAKEMURA <kekeho@sfc.wide.ad.jp>
2022-12-06 07:30:48 +0900

    add: frame of connector

---
+++ packages/caliper-sui/lib/connectorFactory.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const SuiConnector = require('./sui-connector');
+
+/**
+ * Constructs an Sui adapter.
+ * @param {number} workerIndex The zero-based index of the worker who wants to create an adapter instance. -1 for the manager process.
+ * @return {Promise<ConnectorBase>} The initialized adapter instance.
+ * @async
+ */
+async function connectorFactory(workerIndex) {
+    return new SuiConnector(workerIndex, 'ethereum');
+}
+
+module.exports.ConnectorFactory = connectorFactory;
---
+++ packages/caliper-sui/lib/sui-connector.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const {ConnectorBase, CaliperUtils, ConfigUtil, TxStatus} = require('@hyperledger/caliper-core');
+
+class SuiConnector extends ConnectorBase {
+    constructor(workerIndex, bcType) {
+        super(workerIndex, bcType);
+
+        // TODO: Impl
+    }
+}
